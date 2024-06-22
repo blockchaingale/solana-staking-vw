53afe44799509a83c29f244899d22742ed7163e8
unknown <anthonylawson0917@gmail.com>
2024-06-22 18:46:46 +1000

    changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import type { NextComponentType, NextPageContext } from 'next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
