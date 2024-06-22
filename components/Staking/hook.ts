c58aa8ae1e70ac00a4946fac9991f66392addb09
unknown <anthonylawson0917@gmail.com>
2024-06-22 19:26:46 +1000

    changed

--- components/Staking/hook.ts
+++ components/Staking/hook.ts
@@ -49,7 +49,8 @@ export const useHome = () => {
             let globalData = await program.account.globalData.fetchNullable(globalDataPda[0]);
  //           console.log(publicKey.toBase58(), globalData.admin.toBase58())
             if(!globalData || !globalData.admin)return;
-            if(globalData.admin === publicKey.toBase58())return true;
+            console.log(globalData.admin.toBase58())
+            if(globalData.admin.toBase58() === publicKey.toBase58())return true;
             return false;
             // console.log(Admin);
         }
